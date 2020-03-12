d122fb11a99c4f57c63b8d145c351d01d684bac4
edivaldoleitao <52206423+edivaldoleitao@users.noreply.github.com>
2020-03-12 17:54:46 -0300

    Add files via upload

--- AULA.hs
+++ AULA.hs
@@ -25,6 +25,21 @@ maxvendas 0 = vendas 0
 maxvendas n = maxi (maxvendas (n - 1)) (vendas n) 
 
 fib :: Int ->Int 
-fib 0 = 1
+fib 0 = 0
 fib 1 = 1
-fib n = (fib (n - 1)) + (fib (n - 2))
+fib n = (fib (n - 1)) + (fib (n - 2)) 
+
+fat :: Int ->Int
+fat 0 = 1
+fat n = (fat (n - 1)) * n 
+
+all4equal :: Int -> Int -> Int -> Int -> Bool
+all4equal m n o p = (m == n) && ( n == o) && (o == p) 
+
+equalcount :: Int -> Int -> Int -> Int 
+equalcount m n o | (allequal m n o == True) = 3
+                 | m == n = 2
+				 | n == o = 2
+				 | m == o = 2
+				 | otherwise = 0
+
